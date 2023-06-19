769d577c6cfd0e7bfece379831a44eda82be7c6e
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-06-20 02:58:00 +0300

    Adding the timer and clicking events to the game

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
@@ -11,11 +13,41 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthOfSecondsElapsed;
+        int matchesFound;
+
+
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick;
+
             SetUpGame();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            tenthOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthOfSecondsElapsed / 10.0f).ToString("0.0s");
+
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text += " - Play Again?";
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                timeTextBlock.Text = "You WON!!!";
+                Thread.Sleep(2000);
+                SetUpGame();
+            }
         }
 
         void SetUpGame()
@@ -41,6 +73,31 @@ namespace MatchGame
                 string animal = animalEmojis[randomIndex];
                 textBlock.Text = animal;
                 animalEmojis.RemoveAt(randomIndex);
+            }
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool isFindingMatch;
+
+        private void TextBlock_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            if (isFindingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                isFindingMatch = true;
+            }
+            else if (lastTextBlockClicked.Text == textBlock.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                isFindingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                isFindingMatch = false;
             }
         }
     }
